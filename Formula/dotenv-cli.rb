888f913a2bc87f5d9298273d97c779d794802440
Mike Garde <mgarde@gmail.com>
2026-05-11 01:24:36 -0400

    adding formula for dotenv

---
+++ Formula/dotenv-cli.rb
@@ -0,0 +1,38 @@
+class dotenv-cli < Formula
+  desc ""
+  homepage "https://github.com/MikeGarde/dotenv-cli"
+  version "0.0.0"
+  license "GPL-3.0-only"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/MikeGarde/dotenv-cli/releases/download/0.0.0/commitbot-0.0.0-aarch64-apple-darwin.tar.gz"
+      sha256 "00000000000000000000000" # mac arm64
+    end
+
+    on_intel do
+      url "https://github.com/MikeGarde/dotenv-cli/releases/download/0.0.0/commitbot-0.0.0-x86_64-apple-darwin.tar.gz"
+      sha256 "00000000000000000000000" # mac intel
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/MikeGarde/dotenv-cli/releases/download/0.0.0/commitbot-0.0.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "00000000000000000000000" # linux arm64
+    end
+
+    on_intel do
+      url "https://github.com/MikeGarde/dotenv-cli/releases/download/0.0.0/commitbot-0.0.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "00000000000000000000000" # linux intel
+    end
+  end
+
+  def install
+    bin.install "dotenv"
+  end
+
+  test do
+    assert_match "dotenv", shell_output("#{bin}/dotenv --help")
+  end
+end
