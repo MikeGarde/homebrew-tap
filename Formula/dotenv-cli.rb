2e2d98e7ff224b5c57d5b8ecfe564e03ec805732
Mike Garde <mgarde@gmail.com>
2026-07-12 23:41:32 -0400

    Update dotenv-cli to 1.1.2
    - Dropping tar

--- Formula/dotenv-cli.rb
+++ Formula/dotenv-cli.rb
@@ -1,35 +1,35 @@
 class DotenvCli < Formula
   desc "Read and update dotenv files from the cli"
   homepage "https://github.com/MikeGarde/dotenv-cli"
-  version "1.1.1"
+  version "1.1.2"
   license "GPL-3.0-or-later"
 
   on_macos do
     on_arm do
-      url "https://github.com/MikeGarde/dotenv-cli/releases/download/1.1.1/dotenv-cli-1.1.1-apple-darwin-aarch64.tar.gz"
-      sha256 "db2e04b696ce3134fdfc0c9ede7704b227cb6aeb94ddbc53a1b439900ea5bc57" # mac arm64
+      url "https://github.com/MikeGarde/dotenv-cli/releases/download/1.1.2/dotenv-cli-1.1.2-apple-darwin-aarch64.gz"
+      sha256 "6e72fca2839639e61837f25075cc7e613918196ad789324a182742562b08150f" # mac arm64
     end
 
     on_intel do
-      url "https://github.com/MikeGarde/dotenv-cli/releases/download/1.1.1/dotenv-cli-1.1.1-apple-darwin-x86_64.tar.gz"
-      sha256 "1c1a2d40c768c80dc35113627cb278da3c909b1501361c26d1cb0eae29f01444" # mac intel
+      url "https://github.com/MikeGarde/dotenv-cli/releases/download/1.1.2/dotenv-cli-1.1.2-apple-darwin-x86_64.gz"
+      sha256 "df2234ad2800c6fe71a4191a48b91ea57908d9f195fc3905fcd87c1b6827c42e" # mac intel
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/MikeGarde/dotenv-cli/releases/download/1.1.1/dotenv-cli-1.1.1-unknown-linux-gnu-aarch64.tar.gz"
-      sha256 "ed0ed14b1b270d9bf740725daa73c4892088907e31ecc7247c35077f8cbcc388" # linux arm64
+      url "https://github.com/MikeGarde/dotenv-cli/releases/download/1.1.2/dotenv-cli-1.1.2-unknown-linux-gnu-aarch64.gz"
+      sha256 "b53eded8675deaa06c93bb8db54a6f4ff58c3d030a0c16d747604067da913fae" # linux arm64
     end
 
     on_intel do
-      url "https://github.com/MikeGarde/dotenv-cli/releases/download/1.1.1/dotenv-cli-1.1.1-unknown-linux-gnu-x86_64.tar.gz"
-      sha256 "cd14b86593fda886e78a57d04fb12ba48308e64309caa5c43215cd5a99cd16d6" # linux intel
+      url "https://github.com/MikeGarde/dotenv-cli/releases/download/1.1.2/dotenv-cli-1.1.2-unknown-linux-gnu-x86_64.gz"
+      sha256 "54b6cb0ef7f93fd2bbe0de390f76a5b624b7b835317c592c13f74728d2ebcf79" # linux intel
     end
   end
 
   def install
-    bin.install "dotenv"
+    bin.install Dir["dotenv-cli-*"].first => "dotenv"
   end
 
   test do
